7fc75500ce97f5af62ee7aaa231e5665d119937c
Developerzakir <zakir1234@!za>
2026-02-02 21:53:26 +0600

    mongoose db connected

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+dotenv.config()
+
 
 let app = express();
+let port = process.env.PORT || 6000
 
 app.get('/',function(req,res){
     res.send("hello duniya");
 });
 
-app.listen(8000,()=>{
+app.listen(port,()=>{
+    connectDb();
     console.log('server started successfully!');
 });
