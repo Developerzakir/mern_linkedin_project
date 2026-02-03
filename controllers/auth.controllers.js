9a7fb828cea9e056c9a481bb6f8216b97dc51a33
Developerzakir <zakir1234@!za>
2026-02-04 04:11:03 +0600

    user login Api created

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -2,6 +2,9 @@ import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
+
+
+//user signUp function
 export const signUp = async (req,res)=>{
   try{
     let {firstName,lastName,userName,email,password}=req.body
@@ -43,5 +46,36 @@ export const signUp = async (req,res)=>{
   }catch(error){
     return res.status(500).json({message:"sign up error"})
     console.log(error)
+  }
+}
+
+
+//user login function
+export const login = async (req,res)=>{
+  try{
+
+     let {email, password } = req.body;
+
+     let user = await User.findOne({ email });
+     if (!user) {
+       return res.status(400).json({ message: "User does not exists!" });
+     }
+
+     const isMatch = await bcrypt.compare(password,user.password)
+     if(!isMatch){
+      return res.status(400).json({ message: "incorrect password" });
+     }
+
+     let token = genToken(user._id);
+     res.cookie("token", token, {
+       httpOnly: true,
+       maxAge: 7 * 24 * 60 * 60 * 1000,
+       sameSite: "strict",
+       secure: process.env.NODE_ENVIRONMENT === "production",
+     });
+     return res.status(200).json(user);
+
+  }catch(error){
+    return res.status(500).json({message:"login error"})
   }
 }
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signUp } from '../controllers/auth.controllers.js'
+import { login, signUp } from '../controllers/auth.controllers.js'
 
 let authRouter = express.Router()
 
 authRouter.post('/signup',signUp)
+authRouter.post('/login',login)
 
 export default authRouter
