a9841623da3b0fd9607517d597693c23cdf9c220
Developerzakir <zakir1234@!za>
2026-02-07 18:23:35 +0600

    User registration done with Context Api

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import dotenv from 'dotenv'
 import connectDb from './config/db.js';
 import authRouter from './routes/auth.router.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 
 dotenv.config()
 let app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 let port = process.env.PORT || 6000
 
 
--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,10 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import logo from '../assets/logo.png'
 import {useNavigate} from 'react-router-dom'
+import { authDataContext } from '../context/AuthContext'
+import axios from 'axios'
 
 function SignUp() {
   const [showPass, setShowPass] = useState(false)
   const navigate = useNavigate()
+  let {serverUrl} = useContext(authDataContext)
+
+  const [firstName,setFirstName] = useState("")
+  const [lastName,setLastName] = useState("")
+  const [userName,setUserName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+
+  const handleSignUp = async (e)=>{
+    e.preventDefault()
+    try{
+      let result = await axios.post(serverUrl + "/api/auth/signup",{
+        firstName,
+        lastName,
+        userName,
+        email,
+        password
+      },{withCredentials:true})
+      console.log(result)
+      setFirstName("")
+      setLastName("")
+      setUserName("")
+      setEmail("")
+      setPassword("")
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
     <div className="w-full h-screen bg-[white] flex flex-col items-center justify-start gap-[10px]">
       <div className="p-[30px] lg:p-[35px] w-full h-[70px] flex items-center">
@@ -16,7 +49,7 @@ function SignUp() {
       </div>
 
       <form
-        action=""
+        onSubmit={handleSignUp}
         className="
         w-[90%] 
         max-w-[400px]
@@ -28,16 +61,22 @@ function SignUp() {
 
         <h1 className='text-gray-800  text-[30px] font-semibold mb-[30px]'>Sign Up</h1>
 
-        <input type="text" placeholder="firstname" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+        <input type="text" placeholder="firstname" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md'
+        value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
 
-        <input type="text" placeholder="lasttname" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+        <input type="text" placeholder="lasttname" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md'
+         value={lastName} onChange={(e)=>setLastName(e.target.value)} />
 
-        <input type="text" placeholder="username" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+        <input type="text" placeholder="username" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md'
+         value={userName} onChange={(e)=>setUserName(e.target.value)} />
 
-        <input type="email" placeholder="email" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+        <input type="email" placeholder="email" required className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md'
+         value={email} onChange={(e)=>setEmail(e.target.value)} />
 
         <div className='w-[100%] h-[50px] border-2 border-gray-600 text-gray-800 text-[18px] rounded-md relative'>
-          <input type={showPass ? "text" : "password"} placeholder="password" required className='w-full h-full border-none border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md' />
+          <input type={showPass ? "text" : "password"} placeholder="password" required 
+          className='w-full h-full border-none border-gray-600 text-gray-800 text-[18px] px-[20px] py-[10px] rounded-md'
+           value={password} onChange={(e)=>setPassword(e.target.value)} />
           <span  onClick={()=>setShowPass(prev=>!prev)}
           className='absolute right-3 top-2 text-blue-500 font-semibold cursor-pointer'>{showPass?"hidden":"Show"}</span>
         </div>
